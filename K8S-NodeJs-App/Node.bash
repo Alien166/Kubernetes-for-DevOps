82ab318128562b4aef1d2195b93d8e187acfdccf
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2024-07-17 21:35:11 +0300

    Add files via upload

---
+++ K8S-NodeJs-App/Node.bash
@@ -0,0 +1,46 @@
+mkdir K8S-NodeJs-App
+  874  cd K8S-NodeJs-App/
+  875  cd ..
+  876  code K8S-NodeJs-App/
+  877  cd ..
+  878  node -v
+  879  n[m -v
+  880  npm -v
+  881  npm init -y
+  882  clear
+  883  npm install express
+  884  node mjs
+  885  node index.mjs
+  886  docker images ls
+  887  docker images 
+  888  docker images
+  889  kubectl get all
+  890  minikube start
+  891  kubectl get all
+  892  kubectl create deployment k8s-web-serive --image=toba44/k8s-web-service
+  893  kubectl get deployments.apps 
+  894  kubectl get all
+  895  kubectl expose deployment k8s-web-serive --port=3000
+  896  kubectl get all
+  897  kubectl get svc
+  898  kubectl scale deployment k8s-web-serive --replicas=4
+  899  kubectl get po -o wide
+  900  minikube ssh
+  901  kubectl get po -o wide
+  902  minikube ssh
+  903  kubectl get all
+  904  kubectl delete deployments.apps k8s-web-serive 
+  905  kubectl get all
+  906  kubectl expose deployment k8s-web-serive --type=NodePort --port=3000
+  907  kubectl get all
+  908  kubectl create deployment k8s-web-serive --image=toba44/k8s-web-service
+  909  kubectl expose deployment k8s-web-serive --type=NodePort --port=3000
+  910  kubectl get all
+  911  kubectl get svc
+  912  kubectl delete service k8s-web-serive 
+  913  kubectl get svc
+  914  kubectl expose deployment k8s-web-serive --type=NodePort --port=3000
+  915  kubectl get svc
+  916  minikube service k8s-web-serive
+  917  kubectl get svc
+
