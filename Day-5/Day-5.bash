a8861e4106cc4abf441ff7cdda19dc05533e85d0
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2024-07-21 22:20:25 +0300

    Add files via upload

---
+++ Day-5/Day-5.bash
@@ -0,0 +1,43 @@
+cd Day-5
+  984  ls
+  985  kubectl apply -f Deployment.yaml 
+  986  kubectl get po
+  987  kubectl get all
+  988  kubectl apply -f Deployment.yaml 
+  989  kubectl get po
+  990  kubectl describe deployment recreate-deployment 
+  991  kubectl get po
+  992  kubectl apply -f Deployment.yaml 
+  993  kubectl get po
+  994  kubectl apply -f Deployment.yaml 
+  995  kubectl get po
+  996  kubectl get all
+  997  kubectl get po
+  998  minikube service recreateservice
+  999  kubectl apply -f Deployment.yaml 
+ 1000  kubectl get po
+ 1001  kubectl get all
+ 1002  minikube status
+ 1003  minikube start
+ 1004  kubectl get all
+ 1005  minikube service rereateservice
+ 1006  minikube service rereatservice
+ 1007  minikube service recreatservice
+ 1008  kubectl get svc
+ 1009  minikube service recreaetservice
+ 1010  minikube service recreateservice
+ 1011  cd
+ 1012  cd Kubernetes-for-DevOps/
+ 1013  cd Day-5
+ 1014  ls
+ 1015  touch MultiContainerPod.yaml
+ 1016  code MultiContainerPod.yaml 
+ 1017  ls
+ 1018  kubectl apply -f MultiContainerPod.yaml 
+ 1019  kubectl exec -it two-container-pod -c nginx-container --bin/bash
+ 1020  kubectl exec -it two-container-pod -c nginx-container --/bin/bash
+ 1021  kubectl exec -it two-container-pod -c nginx-container -bin/bash
+ 1022  kubectl exec -it two-container-pod -c nginx-container -- /bin/bash
+ 1023  kubectl get po
+ 1024  minikube dashboard
+
